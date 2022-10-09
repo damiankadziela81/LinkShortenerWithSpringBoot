d0f5c8258f34a28f44f4c1edec1924b29340efd5
damiankadziela81 <damiankadziela02@gmail.com>
2022-10-09 16:12:50 +0200

    Added simple frontend  and lombock annotations

--- src/main/java/com/example/linkshortener/controller/RedirectController.java
+++ src/main/java/com/example/linkshortener/controller/RedirectController.java
@@ -1,6 +1,7 @@
 package com.example.linkshortener.controller;
 
 import com.example.linkshortener.link.LinkService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@AllArgsConstructor
 @RestController
 class RedirectController {
 
     private final LinkService linkService;
-
-    RedirectController(final LinkService linkService) {
-        this.linkService = linkService;
-    }
 
 
     @GetMapping(path = "/s/{id}")
